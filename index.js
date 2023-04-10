f9da3f125df60d198fcab53f85da1e141bd68d3e
Matthias Wild <matthias@wildpeter.de>
2023-04-10 19:30:22 +0200

    added new functions

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const crypto = require('crypto');
 const url = require('url');
 const request = require('request');
 const urlencode = require('urlencode');
+const winston = require('winston');
+
+var logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({'timestamp': true}),
+  ],
+});
+
+var verbandsUrl = null;
+var token = null;
 
 exports.printMsg = function() {
   console.log("This is a test message from the verbandonline-node-api package");
@@ -15,18 +30,19 @@ exports.setToken = function(usr, pwd){
   token = generate_token(usr, pwd);
 }
 
-exports.request = function(methodenname, parameter, callback){
+exports.setLogger = function(_logger){
+  logger = _logger;
+}
+
+exports.request = async function(methodenname, parameter, callback){
   if(verbandsUrl == null){
     throw new Error("verbandsURL not set, use setVerbandsUrl(verbandsUrl) first");
   }
   if(token == null){
     throw new Error("token not set, use setToken(username, password) first");
   }
-  return request(verbandsUrl, token, methodenname, parameter, callback);
+  return await _request(verbandsUrl, token, methodenname, parameter, callback);
 }
-
-const verbandsUrl = null;
-const token = null;
 
 function generate_token(usr, pwd){
   return urlencode("A/" + usr + "/" + crypto.createHash('md5').update(pwd).digest('hex'));
@@ -40,11 +56,69 @@ function parameter_to_string(parameter){
   return string;
 }
 
-function request(verbandurl, token, methodenname, parameter, callback){
-  if(!url.endsWith("/")){
-    url += "/";
+async function _request(verbandurl, token, methodenname, parameter, callback){
+  if(!verbandurl.endsWith("/")){
+    verbandurl += "/";
   }
 
   const url = new URL(verbandurl + "?api=" + urlencode(methodenname) + "&token=" + token + parameter_to_string(parameter));
-  return request.get(url, {json: true}, callback);
+  return await request.get(url, {json: true}, callback);
+}
+
+/**
+ * Die Funktion sendet einen API-Request an Verbandonline und testet Nutzername und Passwort.
+ * @param {string} usr 
+ * @param {string} pwd 
+ * @param {callback function(int)} res Diese Funktion wird mit der ID als Paramter aufgerufen, wenn der Login korrekt ist
+ * @param {callback function(string)} err Diese Funktion wird bei einem internen Fehler (500) oder invaliden Login (403) mit einer Fehlermeldung aufgerufen
+ */
+exports.VerifyLoginID = function(usr, pwd, res, err){
+  _request(verbandsUrl, token, 'VerifyLogin', {"user": usr, "password": pwd, "result": "id"}, (request_err, request_res, body) => {
+    if(!request_err && body && body.hasOwnProperty("error")){
+      logger.info("[403] fehlerhafter Login von " + usr + " -> " + body.error);
+      err("[403] fehlerhafter Login von " + usr);
+    } else if(!request_err && body){
+        try {
+            id = parseInt(body[0]);
+            if(id != NaN && id >= 0){
+                // successful login
+                logger.info("[200] Erfolgreicher Login von " + usr);
+                res(id);
+            }
+        } catch (error) {
+            logger.error("[500] invalid response from Stammesmanager for user " + usr);
+            logger.error(error);
+            err("[500] interner Servererror");
+        }
+    } else {
+        logger.error("[500] Error login of " + usr + " -> " + request_err);
+        err("[500] interner Servererror");
+    }
+  });
+}
+
+exports.GetMember = function(id, res, err){
+  _request(verbandsUrl, token, 'GetMember', {"id": id}, (request_err, request_res, body) => {
+    if(!request_err && body && body.hasOwnProperty("error")){
+      logger.error("[500] fehlerhafte Anfrage für ID " + id);
+      logger.error(body.error);
+      err("[500] fehlerhafte Anfrage für ID " + id);
+    } else if(!request_err && body){
+        try {
+            if(body["id"] == id){
+              // successful request
+              logger.info("[200] Erfolgreicher Request für die ID " + id);
+              res(body);
+            }
+        } catch (error) {
+            logger.error("[500] invalid response from Stammesmanager for ID " + id);
+            logger.error(error);
+            err("[500] interner Servererror");
+        }
+    } else {
+        logger.error("[500] Error für ID " + id);
+        logger.error(request_err);
+        err("[500] interner Servererror");
+    }
+  });
 }
